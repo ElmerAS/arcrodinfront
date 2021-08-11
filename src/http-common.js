245545724e4c62e55da0c8762b3fcd2b35ee2528
Elmer <eialfaro29@gmail.com>
2021-08-10 19:39:03 -0500

    url heroku back

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8000/api",
+  //baseURL: "http://localhost:8000/api",
+  baseURL: "https://arcrodinback.herokuapp.com/api",
   headers: {
     "Content-type": "application/json",
   },
